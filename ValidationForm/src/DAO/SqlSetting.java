ae823afa870d5b8d271a494764f57bc03c51712c
koma-git-hub <100895371+koma-git-hub@users.noreply.github.com>
2022-05-01 19:01:20 +0900

    Update SqlSetting.java

--- ValidationForm/src/DAO/SqlSetting.java
+++ ValidationForm/src/DAO/SqlSetting.java
@@ -3,10 +3,10 @@ package DAO;
 public interface SqlSetting {
 	final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	final String URL =
-			"jdbc:mysql://ec2rdsdatabase.c0heqotratpa.ap-northeast-1.rds.amazonaws.com:3306/";
-	final String USER_NAME = "ec2rdsAdmin";
-	final String PASSWORD = "nakadashide19";
-	final String DB_NAME = "chatroom_db";
+			"jdbc:mysql://ec2rdsdatab**********mazonaws.com:3306/";
+	final String USER_NAME = "ec******";
+	final String PASSWORD = "n*******9";
+	final String DB_NAME = "******_db";
 	final String TABLE_NAME = "account";
 	final String[] COLUMNS_NAMES = {"id","name","pass","no"};
 	final String[] COLUMNS_NAME_AS = {"ID","名前","パスワード",""};
